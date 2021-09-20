2bc788f73fafc333797dadfc2983b3386ac454d9
Daniel Gonzalez <daniel.gonzalez@codescience.com>
2021-09-20 14:11:09 -0400

    first import

---
+++ src/main/java/com/codescience/canvasdemo/CanvasDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codescience.canvasdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanvasDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanvasDemoApplication.class, args);
+	}
+
+}
